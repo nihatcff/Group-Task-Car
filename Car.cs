3be854eecfe1cb2b339d9906ed229208a42684d2
Nihat Karimi <nihatrk-map101@code.edu.az>
2025-11-06 19:15:50 +0400

    Latest commit

--- Car.cs
+++ Car.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CarSales
+namespace CarManagementSystem
 {
-    internal class Car
+    public class Car
     {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+
+        public Car(int id, string brand, string model, int year, string color, double price)
+        {
+            Id = id;
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Color = color;
+            Price = price;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"ID: {Id}, Brand: {Brand}, Model: {Model}, Year: {Year}, Color: {Color}, Price: ${Price}");
+        }
     }
 }
+
